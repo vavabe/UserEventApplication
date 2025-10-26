31647f62605fc44442e0c6ddb416f58a97ad20ef
Vavabe <Vavabe@KOMPUTER>
2025-10-26 23:22:18 +0300

    filtering

--- MSDisEventApplication/Services/EventObservable.cs
+++ MSDisEventApplication/Services/EventObservable.cs
@@ -1,5 +1,7 @@
-﻿using MSDisEventApplication.Models;
+﻿using Microsoft.Extensions.ObjectPool;
+using MSDisEventApplication.Models;
 using MSDisEventApplication.Services.Interfaces;
+using System.Reactive.Linq;
 using System.Reactive.Subjects;
 
 namespace MSDisEventApplication.Services;
@@ -7,11 +9,16 @@ namespace MSDisEventApplication.Services;
 public class EventObservable : IEventObservable<UserEvent>, IDisposable
 {
     private readonly Subject<UserEvent> _subject = new Subject<UserEvent>();
+    private readonly string[] _eventFilter = ["hover", "click"];
+    private readonly DateTime dateFromFilter = DateTime.MinValue;
+    private readonly DateTime dateToFilter = DateTime.MaxValue;
     private bool _disposed;
 
     public IDisposable Subscribe(IObserver<UserEvent> observer)
     {
-        return _subject.Subscribe(observer);
+        return _subject
+            .Where(s => _eventFilter.Contains(s.EventType) && s.Timestamp > dateFromFilter && s.Timestamp < dateToFilter)
+            .Subscribe(observer);
     }
 
     public void PublishEvent(UserEvent userEvent)
